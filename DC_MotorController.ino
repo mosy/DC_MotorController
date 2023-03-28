1e0d37d650a3e6c35dfc451f4f42c331da35ab6b
Fred-Olav Mosdal <fred-olav@mosdal.org>
2023-03-28 07:09:55 +0200

    Update DC_MotorController.ino

--- DC_MotorController.ino
+++ DC_MotorController.ino
@@ -1,21 +1,4 @@
-/*
-  Modbus-Arduino Example - Test Holding Register (Modbus IP ESP8266)
-  Configure Holding Register (offset 100) with initial value 0xABCD
-  You can get or set this holding register
-  Original library
-  Copyright by André Sarmento Barbosa
-  http://github.com/andresarmento/modbus-arduino
 
-  Current version
-  (c)2017 Alexander Emelianov (a.m.emelianov@gmail.com)
-  https://github.com/emelianov/modbus-esp8266
-*/
-
-//#ifdef ESP8266
-// #include <ESP8266WiFi.h>
-//#else //ESP32
- #include <WiFi.h>
-//#endif
 #include <ModbusIP_ESP8266.h>
 #include "arduino_secrets.h"
 const int DO1 = 21; //GPIO0
